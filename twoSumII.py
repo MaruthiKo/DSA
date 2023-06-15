20150635ca6c49f07e84270f385ba3be833bbe09
MaruthiKo <marurohi@gmail.com>
2023-06-15 10:32:01 +0530

    Day 4 Q2
    Added Method 2 to Two Sum II question

--- twoSumII.py
+++ twoSumII.py
@@ -1,3 +1,4 @@
+# Method 1
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         numSet = {}
@@ -8,3 +9,18 @@ class Solution:
             else:
                 numSet[numbers[idx1]] = idx1
         return
+
+
+# Method 2
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) -1
+        for num in numbers:
+            total = numbers[l] + numbers[r]
+            if total == target:
+                return [l+1,r+1]
+            elif total > target:
+                r -= 1
+            else:
+                l += 1
+        return 
